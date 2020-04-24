db7738b1e1b80f7b664c1d429fbd3025c077827b
Panupong Chamsomboon <5635512110@psu.ac.th>
2020-04-24 23:25:25 +0700

    chapter 1 - hello world

---
+++ chapter1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << 2016 << " is today's year. "; //<< std::endl;
+    std::cout << 3 << " is today'n moth." << std::endl;
+    std::cout << 16 << " is today's day. " << std::endl;
+
+    return 0;
+}
